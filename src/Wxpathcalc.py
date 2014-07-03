15b277b7b66694d98d6425ba576affda030d7e3f
Jim Richardson <weaselkeeper@gmail.com>
2014-07-03 10:42:16 -0700

    update raiseerror call

--- src/Wxpathcalc.py
+++ src/Wxpathcalc.py
@@ -55,7 +55,7 @@ PROJECTNAME = 'pathcalc'
 try:
     import wx
 except ImportError:
-    raise ImportError, "The wxPython module is required to run this program"
+    raise ImportError("The wxPython module is required to run this program")
 
 # Remaining imports
 import sys
